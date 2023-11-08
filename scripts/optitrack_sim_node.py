8dcb2ec86c71d0aa1b1c8052d7099e1b6fb9f4c3
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-11-08 23:17:42 +0000

    wip twist topic

--- scripts/optitrack_sim_node.py
+++ scripts/optitrack_sim_node.py
@@ -2,8 +2,9 @@
 
 import rclpy
 from rclpy.node import Node
-
+from geometry_msgs.msg import Twist
 from optitrack_sim.msg import OptiData         
+from tf2_py import quaternion_from_euler
 import time
 import yaml
 
@@ -14,21 +15,14 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('optitrack_simulator')
         self.publisher_ = self.create_publisher(OptiData, '/optitrack/data', 10)  
+        self.twist_publisher_ = self.create_publisher(Twist, '/optitrack/cmd_vel', 10)  
         timer_period = 0.5
         self.load_json_data()
-
-    def timer_callback(self):
         
-        msg = OptiData()                                                
-        msg.rigid_bodies = self.i                                           
-        msg.q_drone = [float(self.i)] * 5                                   
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing rigid_bodies: {msg.rigid_bodies} ')       
-        self.get_logger().info(f'Publishing q_drone: {msg.q_drone}')       
-        self.i += 1
-        
-    def load_json_data(self):
+    def load_yaml_data(self):
         file_path = '/root/tello_ros_ws/logger_QDrone.txt'
+        
+        # Dane to kolejno: czy obiekt jest śledzony, pozycja x, pozycja y, pozycja z, kąt roll, kąt pitch, kąt yaw. Pozycja x,y,z jest liczona od środka laboratorium (punkt 0,0,0).
 
         with open(file_path,'r') as file:
             for line in file:
@@ -40,6 +34,19 @@ class MinimalPublisher(Node):
                 msg.rigid_bodies = rigid_bodies                         
                 msg.q_drone = q_drone
                 self.publisher_.publish(msg)
+                
+                twist_msg = Twist()
+                twist_msg.linear.x = rigid_bodies[0]
+                twist_msg.linear.y = rigid_bodies[1]
+                twist_msg.linear.z = rigid_bodies[2]
+                
+                quaternion = quaternion_from_euler(rigid_bodies[3],rigid_bodies[4],rigid_bodies[5])
+                twist_msg.angular.x = quaternion[0]
+                twist_msg.angular.x = quaternion[1]
+                twist_msg.angular.x = quaternion[2]
+                self.twist_publisher_.publish(twist_msg)
+                
+                
                 time.sleep(0.5)
                 
         
