0aa7fbc274f0f78355435cf62e9a93ef8b7432dd
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-11-15 22:27:29 +0000

    updated node - listening to udp server

--- scripts/optitrack_sim_node.py
+++ scripts/optitrack_sim_node.py
@@ -4,52 +4,39 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from optitrack_sim.msg import OptiData         
-from tf2_py import quaternion_from_euler
 import time
 import yaml
+import socket
 
-
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('optitrack_simulator')
-        self.publisher_ = self.create_publisher(OptiData, '/optitrack/data', 10)  
         self.twist_publisher_ = self.create_publisher(Twist, '/optitrack/cmd_vel', 10)  
-        timer_period = 0.5
-        self.load_json_data()
+        self.receive_from_server()
         
-    def load_yaml_data(self):
-        file_path = '/root/tello_ros_ws/logger_QDrone.txt'
-        
-        # Dane to kolejno: czy obiekt jest śledzony, pozycja x, pozycja y, pozycja z, kąt roll, kąt pitch, kąt yaw. Pozycja x,y,z jest liczona od środka laboratorium (punkt 0,0,0).
+    def receive_from_server(self):
+        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        sock.bind((UDP_IP,UDP_PORT))
 
-        with open(file_path,'r') as file:
-            for line in file:
-                data = yaml.safe_load(line)
-                rigid_bodies = data['rigid_bodies']
-                q_drone = [float(i) for i in data['QDrone']]
-                
-                msg = OptiData()                                                
-                msg.rigid_bodies = rigid_bodies                         
-                msg.q_drone = q_drone
-                self.publisher_.publish(msg)
-                
-                twist_msg = Twist()
-                twist_msg.linear.x = rigid_bodies[0]
-                twist_msg.linear.y = rigid_bodies[1]
-                twist_msg.linear.z = rigid_bodies[2]
-                
-                quaternion = quaternion_from_euler(rigid_bodies[3],rigid_bodies[4],rigid_bodies[5])
-                twist_msg.angular.x = quaternion[0]
-                twist_msg.angular.x = quaternion[1]
-                twist_msg.angular.x = quaternion[2]
-                self.twist_publisher_.publish(twist_msg)
-                
-                
-                time.sleep(0.5)
-                
-        
+        while True:
+            data,addr = sock.recvfrom(1024)
+            decoded_data = data.decode('utf-8')
+            received_object = yaml.safe_load(decoded_data)
+            
+            rigid_bodies = [float(i) for i in received_object['QDrone']]
+            twist_msg = Twist()
+            twist_msg.linear.x = rigid_bodies[0]
+            twist_msg.linear.y = rigid_bodies[1]
+            twist_msg.linear.z = rigid_bodies[2]
+            
+            twist_msg.angular.x = rigid_bodies[3]
+            twist_msg.angular.y = rigid_bodies[4]
+            twist_msg.angular.z = rigid_bodies[5]
+            self.twist_publisher_.publish(twist_msg)
 
 
 def main(args=None):
